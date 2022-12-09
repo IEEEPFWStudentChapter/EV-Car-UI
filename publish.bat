ecf5fa73775948ebe58efffbe6d5cad790bd3e8f
Hussain <hussain.azizsaif@gmail.com>
2022-12-09 17:57:14 -0500

    change build to 64 bit rpi os

--- publish.bat
+++ publish.bat
@@ -1,12 +1,12 @@
 cd "EV Car UI"
 dotnet publish -r win-x64 -p:Configuration=Release -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true
-dotnet publish -r linux-arm -p:Configuration=Release -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true
+dotnet publish -r linux-arm64 -p:Configuration=Release -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:UseAppHost=true --self-contained true
 
 cd ..
 
 rm output/*.zip
 
-copy "run.sh" "./EV Car UI/bin/Release/net6.0/linux-arm/publish/"	
+copy "run.sh" "./EV Car UI/bin/Release/net6.0/linux-arm64/publish/"	
 
-7z a "output/linux.zip" "./EV Car UI/bin/Release/net6.0/linux-arm/publish/*"
+7z a "output/rasberrypi.zip" "./EV Car UI/bin/Release/net6.0/linux-arm64/publish/*"
 7z a "output/windows.zip" "./EV Car UI/bin/Release/net6.0/win-x64/publish/*"
